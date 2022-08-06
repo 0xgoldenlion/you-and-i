1efc691a39c6fdffeeaaa22ddca4083625c4db4e
0xgoldenlion <0xgoldenlion@gmail.com>
2022-08-06 08:17:23 +0300

    updated nav

--- client/components/footer.tsx
+++ client/components/footer.tsx
@@ -3,14 +3,7 @@ export const Footer = () => {
         <div className="mt-auto w-full z-40 pb-10">
             <div className="h-5 flex justify-center">
                 <i className="opacity-30">
-                    You{" "}
-                    <a className="underline underline-offset-2 " href="https://github.com/amirgamil/thalia">
-                        experiment
-                    </a>{" "}
-                    and {" "}
-                    <a className="underline underline-offset-2 " href="https://twitter.com/amirbolous">
-                        I
-                    </a>{" "}
+                    You & I {" "}
                     inspired by{" "}
                     <a className="underline underline-offset-2 " href="https://typatone.com/">
                         Typeatone
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import * as React from "react";
 import Head from "next/head";
+import { Playground } from "../components/playground";
 import styles from "../styles/Home.module.css";
 import { Nav } from "../components/nav";
 import { Footer } from "../components/footer";
@@ -34,7 +35,7 @@ const Home: NextPage = () => {
     return (
         <div className={styles.container}>
             <Head>
-                <title>Thalia</title>
+                <title>You & I </title>
                 <meta name="description" content="On-chain music composition" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
@@ -42,7 +43,7 @@ const Home: NextPage = () => {
             <Nav />
             <main className={styles.main}>
                 <div className="py-4">
-                    <h1 className="text-xl font-bold">🎵You and I</h1>
+                    <h1 className="text-xl font-bold">🎵 You & I  Playground</h1>
                     <p className="text-xs opacity-50">
                         Tell stories with sound, text, and shapes all at once. Oh and it&apos;s all on-chain.
                     </p>
@@ -51,7 +52,13 @@ const Home: NextPage = () => {
                     <strong>On-chain music composition</strong>.<br></br>Compose tunes with anyone and everyone{" "}
                     <strong>straight from your keyboard</strong>.
                 </p>
-               
+                <Playground
+                    isLoading={false}
+                    prevMusicNotes={""}
+                    updateSongCallback={updateSongCallback}
+                    rawNotes={rawNotes}
+                    bpm={150}
+                />
                 <div className="relative mt-auto pb-5">
                     <Button onClick={(evt) => prepareToMakeSong()}>Make this song!</Button>
                     <Footer />
